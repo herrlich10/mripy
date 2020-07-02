ce79a7745f73a03965d35051bd71f8b923402277
herrlich10 <herrlich10@gmail.com>
2020-07-02 17:44:36 +0800

    Clean up old scripts before uploading to github.
    Their functionality is mostly taken over by modules or new scripts.

--- scripts/setup_ipython.sh
+++
@@ -1,3 +0,0 @@
-source switch_python.sh default
-sudo easy_install pip
-sudo pip install ipython
--- scripts/upload_mripy.sh
+++
@@ -1,5 +0,0 @@
-# Get package path
-PACKAGE_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}" )" ; cd ..; pwd -P )"
-
-rm -r "/Volumes/FTP/sample programs/qcc/mripy"
-cp -r $PACKAGE_PATH "/Volumes/FTP/sample programs/qcc/mripy"
